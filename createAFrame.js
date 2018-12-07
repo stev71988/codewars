2b9d254064c701d6d2b73dd43fd43fb909b4c92e
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2018-12-07 10:16:47 -0500

    refactored code

--- createAFrame.js
+++ createAFrame.js
@@ -1,30 +1,24 @@
 const frame = (text, char) => {
     let finalFrame = '';
-    let maxLength = Math.max(...text.map(elem => elem.replace(elem, elem.length))) + 4;
-    
+
+    // Find the max width
+    let maxWidth = 0;
+    for (let word of text) {
+        if (word.length > maxWidth) {
+            maxWidth = word.length + 4
+        }
+    }
+
     // Build the top and bottom frames
-    let topBottom = char.repeat(maxLength);
-    // for (let i=0; i<maxLength; i++) {
-    //     topBottom += char;
-    // }
+    let topBottom = char.repeat(maxWidth);
 
     // Build the body of the frame
-    let body = '';
-    for (let j=0; j<text.length; j++) {
-        let currentWord = text[j];
-        let row = '';
-
-        if (currentWord.length + 4 < maxLength) {
-            let buffer = maxLength - (currentWord.length + 4);
-            let space = ''
-            for (let k=0; k<=buffer; k++) {
-                space += ' ';
-            }
-            row = char + ' ' + currentWord + space + char + '\n';
-        } else {
-            row = char + ' ' + currentWord + ' ' + char + '\n';
-        }
-        body += row;
+    let left = char + ' ';
+    let right = ' ' + char + '\n';
+    let body = ''
+    for (let word of text) {
+        let buffer = ' '.repeat(maxWidth - (word.length+4))
+        body +=  left + word + buffer + right;
     }
 
     return topBottom + '\n' + body + topBottom;
