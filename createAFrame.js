3583b1e9b572c91c15606e71b248c0ac79d735e3
stev71988 <stephensim88@gmail.com>
2018-12-07 08:48:14 -0500

    Create createAFrame.js

---
+++ createAFrame.js
@@ -0,0 +1,31 @@
+const frame = (text, char) => {
+    let finalFrame = '';
+    let maxLength = Math.max(...text.map(elem => elem.replace(elem, elem.length))) + 4;
+    
+    // Build the top and bottom frames
+    let topBottom = '';
+    for (let i=0; i<maxLength; i++) {
+        topBottom += char;
+    }
+
+    // Build the body of the frame
+    let body = '';
+    for (let j=0; j<text.length; j++) {
+        let currentWord = text[j];
+        let row = '';
+
+        if (currentWord.length + 4 < maxLength) {
+            let buffer = maxLength - (currentWord.length + 4);
+            let space = ''
+            for (let k=0; k<=buffer; k++) {
+                space += ' ';
+            }
+            row = char + ' ' + currentWord + space + char + '\n';
+        } else {
+            row = char + ' ' + currentWord + ' ' + char + '\n';
+        }
+        body += row;
+    }
+
+    return topBottom + '\n' + body + topBottom;
+};
