fdb1c09c12a60f1c026533e7dc9e0902fc5d33a9
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2018-12-07 11:18:02 -0500

    added comments to code

--- findUniqueNumber.js
+++ findUniqueNumber.js
@@ -1,6 +1,7 @@
 const findUniq = (array) => {
     let unique = {};
 
+    // Create an object where the key = number and value = the count
     for (let number of array) {
         if (unique[number] === undefined) {
             unique[number] = 1;
@@ -8,8 +9,10 @@ const findUniq = (array) => {
             unique[number] += 1;
         }
     }
+    // Search through object and return the key where value === 1
     for (let key in unique) {
         if (unique[key] === 1) {
+            
             return parseFloat(key)
         }
     }
