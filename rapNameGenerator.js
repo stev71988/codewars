8f945f6177cb59f7ceca1bc5d0254f413da77a76
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2018-12-10 18:12:54 -0500

    refactored code

--- rapNameGenerator.js
+++ rapNameGenerator.js
@@ -1,33 +1,6 @@
 const rapNameGen = (str) => {
-    let rapName = [];
-    let splitStr = str.toLowerCase().split('.');
-
-    // Function that finds average
-    let average = (arr) => {
-        let splitVar = arr.split('');
-        let sum = parseInt(splitVar[0]) + parseInt(splitVar[1]);
-
-        return Math.round(sum/2)
-    }
-
-    let day = average(splitStr[0]);
-    let year = average(splitStr[2]);
-
-    let numberList = {
-        0: '0ero',
-        1: '1ne',
-        2: '2wo',
-        3: '3hree',
-        4: '4our',
-        5: '5ive',
-        6: '6ix',
-        7: '7even',
-        8: '8ight',
-        9: '9ine'
-    }
-
-    rapName.push(numberList[day])
-    rapName.push(numberList[year])
-
-    return rapName.join(' ');
+    const names = ['0ero', '1ne', '2wo', '3hree', '4our', '5ive', '6ix', '7even', '8ight', '9ine'];
+    const [day, month, year] = str.split('.');
+    const avg = num => Math.round((+num[0] + +num[1]) / 2)
+    return names[avg(day)] + ' ' + names[avg(year)];
 }
