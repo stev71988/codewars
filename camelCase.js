2e606d830ccd389508c6da87dd0c57dfbeee9d4b
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2018-12-10 21:54:05 -0500

    refactored solution using .map & index

--- camelCase.js
+++ camelCase.js
@@ -2,10 +2,3 @@ const toCamelCase = (str) => {
     return str.split(/[-_]/).map((word, index) =>
     (index === 0) ? word : word.replace(word[0], word[0].toUpperCase())).join('')
 }
-// const toCamelCase = (str) => {
-//     return str.split(/[-_]/).map((word, index) =>
-//     index === 0 ? word : word.replace(word[0], word[0].toUpperCase()))
-//     .join('')
-// }
-
-console.log(toCamelCase('the-stealth-warrior'))
