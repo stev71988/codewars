00b57e119ddffa85a3c797a439ff3a79351a7ac8
stev71988 <stephensim@Stephens-MBP.lan>
2018-12-13 12:52:51 -0500

    refactored with cleaner return statement and deleted test code

--- coding_meetup_4.js
+++ coding_meetup_4.js
@@ -1,20 +1,4 @@
 const getFirstPython = (array) => {
     let pythonDevs = array.find( dev => dev.language === 'Python')
-    return (pythonDevs === undefined) ? 'There will be no Python developers' : pythonDevs.firstName + ', ' + pythonDevs.country;
+    return pythonDevs ? `${pythonDevs.firstName}, ${pythonDevs.country}` : 'There will be no Python Developers';
 }
-
-
-var list1 = [
-    { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
-    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
-    { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
-  ];
-  
-  var list2 = [
-    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
-    { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
-  ];
-  
-  console.log(getFirstPython(list1)) // 'Victoria, Puerto Rico');
-  console.log(getFirstPython(list2)) // 'There will be no Python developers');
-  
