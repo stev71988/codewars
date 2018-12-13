c85e6417c869dfe5c2cf9216e303aea953d53134
stev71988 <stephensim@Stephens-MBP.lan>
2018-12-13 13:11:45 -0500

    refactored code using only for loop with OR statement; deleted test code

--- coding_meetup_5.js
+++ coding_meetup_5.js
@@ -1,20 +1,7 @@
 const countLanguages = (array) => {
-    let languageList = array.map( dev => dev.language)
-    let totalCount = {}
-    for (let language of languageList) {
-        (totalCount[language] === undefined) ? totalCount[language] = 1 : totalCount[language] += 1;
+    let count = {};
+    for (let developer of array) {
+        count[developer.language] = (count[developer.language] || 0) + 1;
     }
-    return totalCount;
+    return count;
 }
-
-
-var list1 = [
-    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
-    { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
-    { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
-    { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
-  ];
-  
-  var answer = { C: 2, JavaScript: 1, Ruby: 1 };
-  
-  console.log(countLanguages(list1)) //
