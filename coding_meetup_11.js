22dd4a64e864de1acede96d2c44335162b439bd5
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2018-12-17 11:48:57 -0500

    refactored using arrow function

--- coding_meetup_11.js
+++ coding_meetup_11.js
@@ -1,21 +1,3 @@
-const getAverageAge = (array) => {
-    return Math.round(array.reduce(function(sum, value) {
-        return sum + value.age
-    }, 0) / array.length)
+const getAverageAge = array => {
+    return Math.round(array.reduce((sum, value) => sum + value.age, 0)/array.length)
 }
-
-
-var list1 = [
-    { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
-    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' }
-  ];
-  
-console.log(getAverageAge(list1)) //, 50)
-
-var list2 = [
-    { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 20, language: 'Ruby' },
-    { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 21, language: 'C' },
-  ];
-  
-console.log(getAverageAge(list2)) //, 21);
-  
