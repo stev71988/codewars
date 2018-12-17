6b244c1a8c603e13589b35a74754dbded45c584b
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2018-12-17 15:53:58 -0500

    removed test code

--- coding_meetup_15.js
+++ coding_meetup_15.js
@@ -16,25 +16,3 @@ const findOddNames = (array) => {
     })
     return answer;
 }
-
-
-var list1 = [
-    { firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
-    { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
-  ];
-  
-  var answer1 = [
-    { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
-  ];
-  
-  console.log(findOddNames(list1)) //, answer1);
-  
-  
-  var list2 = [
-    { firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
-  ];
-  
-  var answer2 = [];
-  
-  console.log(findOddNames(list2)) //, answer2);
-  
