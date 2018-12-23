3be2d5a54eee24526d7916ed5c455cc6a3a08f2c
stev71988 <stephensim@Stephens-MacBook-Pro.local>
2018-12-23 10:50:10 -0500

    used multiple map methods in one

--- toWeirdCase.js
+++ toWeirdCase.js
@@ -1,11 +1,7 @@
 const toWeirdCase = (str) => {
-    let words = str.toLowerCase().split(' ')
-    let answer = [];
-    for (let word of words) {
-        let char = word.split('');
-        answer.push(char.map((x, i) => (i%2 === 0) ? x.toUpperCase() : x).join('')) 
-    }
-    return answer.join(' ')
+    return str.toLowerCase().split(' ').map(word => {
+        return word.split('').map((char, index) => {
+            return index % 2 == 0 ? char.toUpperCase() : char.toLowerCase();
+        }).join('')
+    }).join(' ')
 }
-
-console.log(toWierdCase("This is a test"))
