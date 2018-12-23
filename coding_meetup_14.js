7ecc741a660156eba5283e04707dea0e41498595
stev71988 <stephensim@Stephens-MacBook-Pro.local>
2018-12-23 10:49:36 -0500

    added reduce method

--- coding_meetup_14.js
+++ coding_meetup_14.js
@@ -3,3 +3,11 @@ const orderFood = (array) => {
     array.forEach(x => mealCount[x.meal] = (mealCount[x.meal] || 0) +1)
     return mealCount
 }
+
+// // same function using reduce method
+// const orderFood = (array) => {
+//     return array.reduce((meals, dev) => {
+//         meals[dev.meal] = (meals[dev.meal] || 0) +1;
+//         return meals
+//     }, {})
+// }
