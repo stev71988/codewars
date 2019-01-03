67f7300bb9f6ab25c219b2efa6348443cd125afe
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2019-01-03 09:42:58 -0500

    refactored using Every method

--- isPangram.js
+++ isPangram.js
@@ -1,16 +1,6 @@
 const isPangram = (string) => {
-    let letters = 'abcdefghijklmnopqrstuvwxyz';
-    string = string.replace(/[^\w]/gi, '').toLowerCase()
-    for (let char of letters) {
-        if (string.indexOf(char) === -1) return false;
-    }
-    return true;
+    string = string.toLowerCase();
+    return "abcdefghijklmnopqrstuvwxyz".split('').every(x => {
+        return string.indexOf(x) !== -1;
+    })
 }
-
-
-
-var string1 = "The quick brown fox jumps over the lazy dog."
-console.log(isPangram(string1)) //, true)
-var string2 = "This is not a pangram."
-console.log(isPangram(string2)) //, false)
-
